79197be485bd889f9a63dc950812f8d588370f19
Julien NICOLAS <jnicolas@online.net>
2020-02-17 23:30:12 +0100

    logger - add caller info

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -10,6 +10,7 @@ import (
 // New creates a new logger. Do so if you may log from another go routine
 func New() *logrus.Logger {
 	log := logrus.New()
+	log.ReportCaller = true
 	file, err := os.OpenFile(config.DefaultAppErrorLogFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		log.Info("Failed to log to file, using default stderr")
