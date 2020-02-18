f63c751539174991eb83d97c7e071a988848b7aa
Julien NICOLAS <jnicolas@online.net>
2020-02-18 20:42:53 +0100

    create a cli for the app

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,9 +1,32 @@
 package app
 
-import "github.com/Juli3nnicolas/http_log_monitor/pkg/config"
+import (
+	"time"
+)
+
+// Config is a struct to initialise the application
+type Config struct {
+	// appErrorLogFile is the path to the app's error log file
+	AppErrorLogFile string
+	// logFilePath refers to the first file the app will try to read the logs from
+	LogFilePath string
+	BackendConfig
+}
+
+// BackendConfig is a struct to initailise the app's backend
+type BackendConfig struct {
+	// updateFrameDuration refers to the default time the app will carry out all its measures
+	// Said diferently, this value defines the app's backend refresh rate
+	UpdateFrameDuration time.Duration
+	// alertFrameDuration corresponds to a time-frame during which work related to a single alert will be carried to compute its state
+	// It's an alert refreshing rate
+	AlertFrameDuration time.Duration
+	// alertThreshold is the default threshold (in req/s) triggering an alert
+	AlertThreshold uint64
+}
 
 // Run executes the entire application (both frontend and backend)
-func Run() error {
+func Run(conf *Config) error {
 	// Init backend
 	b := Backend{}
 	if err := b.init(); err != nil {
@@ -21,7 +44,8 @@ func Run() error {
 
 	updateChan := make(chan ViewFrame)
 	go r.update(updateChan, LogUpdateError(""))
-	go b.run(config.DefaultUpdateFrameDuration, updateChan)
+	go b.run(conf.UpdateFrameDuration, updateChan)
+	//go b.run(&conf.BackendConfig, updateChan)
 
 	err = r.render(ctx)
 	if err != nil {
--- pkg/main.go
+++ pkg/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/Juli3nnicolas/http_log_monitor/pkg/app"
+import (
+	"github.com/Juli3nnicolas/http_log_monitor/pkg/app"
+	"github.com/Juli3nnicolas/http_log_monitor/pkg/config"
+	"github.com/spf13/cobra"
+)
+
+var conf *app.Config = &app.Config{}
 
 func main() {
-	app.Run()
+
+	var rootCmd = &cobra.Command{Use: "logmonitor",
+		Short: "Monitors a log file following the common log format",
+		Long: `Monitors a log file following the common log format
+It displays various metrics such as the request-rate, the number of
+successful and failed calls, live details of http-return-codes
+and the website with the most traffic. An alerting feature is also
+present to be notified when traffic gets awry.`,
+		Args: cobra.MaximumNArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
+			app.Run(conf)
+		},
+	}
+
+	rootCmd.Flags().StringVarP(&conf.AppErrorLogFile, "error", "e", config.DefaultAppErrorLogFile, "path to the error log file")
+	rootCmd.Flags().StringVarP(&conf.LogFilePath, "path", "p", config.DefaultLogFilePath, "path to the log file to monitor traffic from")
+	rootCmd.Flags().DurationVarP(&conf.UpdateFrameDuration, "update", "u", config.DefaultUpdateFrameDuration, "app's refresh rate - rate at which data are going to be fetched and displayed")
+	rootCmd.Flags().DurationVarP(&conf.AlertFrameDuration, "alert-period", "T", config.DefaultAlertFrameDuration, "configure alerts' monitoring interval - if the request-rate is above it fro -T, an alert is given")
+	rootCmd.Flags().Uint64VarP(&conf.AlertThreshold, "alert-threshold", "t", config.DefaultAlertThreshold, "threshold value, if the request rate is above for -T time, an alert is switched on")
+	rootCmd.Execute()
 }
